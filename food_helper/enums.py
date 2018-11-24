4b85ff75609d59b01db60d9afc79fb460afd2703
Elliot Greenlee <egreenle@vols.utk.edu>
2018-11-24 08:29:00 -0600

    setup basic cuisine and food type enums

---
+++ food_helper/enums.py
@@ -0,0 +1,20 @@
+"""
+Elliot Greenlee
+
+11/23/2018
+
+enums.py
+"""
+
+from enum import Enum
+
+
+class Cuisine(Enum):
+    mexican = 1
+    asian = 2
+
+
+class RecipeType(Enum):
+    entree = 1
+    side = 2
+
