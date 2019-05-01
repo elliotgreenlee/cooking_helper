f2ab94a95a1b1b732360540fdbdaad3a312e575b
Elliot Greenlee <egreenle@vols.utk.edu>
2019-04-30 20:23:39 -0700

    add amount and units to ingredient class

--- food_helper/ingredient.py
+++ food_helper/ingredient.py
@@ -10,10 +10,12 @@ from food_helper.tools import generate_id
 
 
 class Ingredient:
-    def __init__(self, name, description):
+    def __init__(self, name, description, amount, units):
         self.id = generate_id(name)  # ingredient identification string
         self.name = name  # name of the ingredient
         self.description = description  # description of the ingredient
+        self.amount = amount  # amount of the ingredient in units
+        self.units = units  # unit for the amount of the ingredient
         
         self.seasons = set()  # season of the year
         
